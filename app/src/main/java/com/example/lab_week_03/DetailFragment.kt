eb05932d30dc3a91d4268fe618d1abd90f3ec638
Ahmad Dhavi <ahmaddhavi21@gmail.com>
2025-09-15 15:33:19 +0700

    Commit No. 3 – use dynamic fragment

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -25,6 +25,12 @@ class DetailFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
+
 
     fun setCoffeeData(id: Int) {
         when (id) {
@@ -44,7 +50,12 @@ class DetailFragment : Fragment() {
     }
 
     companion object {
-        @JvmStatic
-        fun newInstance() = DetailFragment()
+        private const val COFFEE_ID = "COFFEE_ID"
+        fun newInstance(coffeeId: Int) =
+            DetailFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(COFFEE_ID, coffeeId)
+                }
+            }
     }
 }
