6f2e8b6cd91ac701e4daa26accfd4a7974e2ede1
Ahmad Dhavi <ahmaddhavi21@gmail.com>
2025-09-15 16:16:26 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -9,14 +9,8 @@ import androidx.fragment.app.Fragment
 
 class DetailFragment : Fragment() {
 
-    private val coffeeTitle: TextView?
-        get() = view?.findViewById(R.id.coffee_title)
-    private val coffeeDesc: TextView?
-        get() = view?.findViewById(R.id.coffee_desc)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var coffeeTitle: TextView
+    private lateinit var coffeeDesc: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,37 +19,31 @@ class DetailFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+
+        coffeeTitle = view.findViewById(R.id.coffee_title)
+        coffeeDesc = view.findViewById(R.id.coffee_desc)
+
+        val coffeeId = arguments?.getInt(ListFragment.COFFEE_ID, 0) ?: 0
         setCoffeeData(coffeeId)
     }
 
-
-    fun setCoffeeData(id: Int) {
+    private fun setCoffeeData(id: Int) {
         when (id) {
             R.id.affogato -> {
-                coffeeTitle?.text = getString(R.string.affogato_title)
-                coffeeDesc?.text = getString(R.string.affogato_desc)
+                coffeeTitle.text = getString(R.string.affogato_title)
+                coffeeDesc.text = getString(R.string.affogato_desc)
             }
             R.id.americano -> {
-                coffeeTitle?.text = getString(R.string.americano_title)
-                coffeeDesc?.text = getString(R.string.americano_desc)
+                coffeeTitle.text = getString(R.string.americano_title)
+                coffeeDesc.text = getString(R.string.americano_desc)
             }
             R.id.latte -> {
-                coffeeTitle?.text = getString(R.string.latte_title)
-                coffeeDesc?.text = getString(R.string.latte_desc)
+                coffeeTitle.text = getString(R.string.latte_title)
+                coffeeDesc.text = getString(R.string.latte_desc)
             }
         }
-    }
-
-    companion object {
-        private const val COFFEE_ID = "COFFEE_ID"
-        fun newInstance(coffeeId: Int) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(COFFEE_ID, coffeeId)
-                }
-            }
     }
 }
